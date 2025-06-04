df15257a265405306e34afa4361db03f76726416
Faris Hadzisadikovic <hadzisadikovicfaris@gmail.com>
2025-06-04 16:36:13 -0400

    Final push

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,9 +274,9 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/paradise-nursery-shopping-cart-app/" style={{ textDecoration: 'none' }}>
+                        <a href="/e-plantShopping/paradise-nursery-shopping-cart-app/" style={{ textDecoration: 'none' }}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Faris's Nursery</h3>
+                                <h3 style={{ color: 'white' }}>Faris' Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                             </div>
                         </a>
